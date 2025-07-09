311cedf7a684aaea5ffcd52906902d608b074627
Claudiasals <salsiniclaudia@gmail.com>
2025-07-09 15:45:51 +0200

    ES.1 JS

--- JAVASCRIPT/ESERCIZI/es-piattaforma.js
+++ JAVASCRIPT/ESERCIZI/es-piattaforma.js
@@ -1,10 +1,9 @@
 
+//ESERCIZIO 1
 const età = 8;
 let giorno = "mercoledì";
 console.log(età, giorno);
 
-let frutta = "banana";
-console.log(frutta);
-frutta = "fragola";
-console.log(frutta);
+
+
 
