564107d5bab8ee843d9a679fc04f2749e82f924f
Claudiasals <salsiniclaudia@gmail.com>
2025-07-09 18:19:19 +0200

    es.5

--- JAVASCRIPT/ESERCIZI/es-piattaforma.js
+++ JAVASCRIPT/ESERCIZI/es-piattaforma.js
@@ -21,12 +21,24 @@ console.log(number);
 
 /*Sono due entità differenti*/
 
+//ESERCIZIO 4
 let outside = 8;
 {
     let inside = 9;
     console.log(outside)
     console.log(inside)
 }
-console.log(outside)
-console.log(inside)
-/* la stampa di inside dall'esterno non viene eseguita xké inside è definito solo all'interno del blocco*/
+
+/* la stampa di inside dall'esterno non viene eseguita 
+xké inside è definito solo all'interno del blocco*/
+
+//ESERCIZIO 5
+const evenNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+for (let i = 0; i < evenNumbers.length; i++) {
+    if (evenNumbers[i] % 2 === 0) { /* Se il numero in posizione i è pari, 
+        allora esegui il codice dentro le parentesi { ... }, 
+        in qst caso il console.log, quindi stampa */
+    
+      console.log(evenNumbers[i]);
+}
+}
