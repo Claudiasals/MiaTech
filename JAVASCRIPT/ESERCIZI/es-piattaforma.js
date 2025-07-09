b2b6917bd7116fbfb826f5c2e9b19387daee3645
Claudiasals <salsiniclaudia@gmail.com>
2025-07-09 15:59:56 +0200

    es.3

--- JAVASCRIPT/ESERCIZI/es-piattaforma.js
+++ JAVASCRIPT/ESERCIZI/es-piattaforma.js
@@ -10,3 +10,14 @@ let frutta = "banana";
 console.log(frutta);
 frutta = "fragola";
 console.log(frutta);
+
+//ESERCIZIO 3
+let number = 12;
+{
+    let number = 4;
+    console.log(number);
+}
+console.log(number);
+
+/*Viene stampata prima la variablie all'interno del blocco*/
+
