751129f7670be6a2e1b3caca5af5261b1b5bf3e1
Claudiasals <salsiniclaudia@gmail.com>
2025-07-11 00:39:54 +0200

    es.11

--- JAVASCRIPT/ESERCIZI/es10.js
+++ JAVASCRIPT/ESERCIZI/es10.js
@@ -1,4 +1,5 @@
 //ESERCIZIO 10
+/*
 let number = 3; {
     if (number>5){
         console.log("La variabile è maggiore di 5");
@@ -6,4 +7,25 @@ let number = 3; {
         console.log("La variabile è minore di 5");
     }
 }
+*/
+//ESERCIZIO 11
+//Metodo parseInt
+let score = parseInt(prompt("scrivi un numero:"));
+if (score < 59) {
+    document.write("voto insufficiente");
+} else if (score >= 60 && score <= 69) {
+    document.write("voto sufficiente");
+} else if (score >= 70 && score <= 89) {
+    document.write("voto buono")
+} else if (score >= 90 && score <= 100) {
+    document.write("voto ottimo")
+}
 
+/* 
+"&&" significa "e" 
+&& si può usare con qualsiasi valore booleano 
+(vero/falso), quindi:
+- Numeri (con confronti): score >= 60 && score <= 69
+- Stringhe (con condizioni): (str === "ciao" && str.length > 3)
+- Variabili booleane: (isLoggedIn && isAdmin)
+*/
