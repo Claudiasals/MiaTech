00895c6d9570f44a841e9b613af3caab18e50f48
Claudiasals <salsiniclaudia@gmail.com>
2025-07-10 18:54:19 +0200

    esercizio 6

--- JAVASCRIPT/ESERCIZI/es-piattaforma.js
+++ JAVASCRIPT/ESERCIZI/es-piattaforma.js
@@ -33,13 +33,58 @@ let outside = 8;
 xké inside è definito solo all'interno del blocco*/
 
 //ESERCIZIO 5
-const evenNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+const evenNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 for (let i = 0; i < evenNumbers.length; i++) {
     if (evenNumbers[i] % 2 === 0) { /* Se il numero in posizione i è pari, 
         allora esegui il codice dentro le parentesi { ... }, 
         in qst caso il console.log, quindi stampa.
-        %2 === 0 significa che il numero è divisibile per 2 con resto di 0 */
-    
-      console.log(evenNumbers[i]);
+        %2 === 0 significa che il numero è divisibile per 2 con resto di 0.
+        riga++ significa che il valore si adrà a sommare all'attuale totale ad ogni ciclo. */
+
+        console.log(evenNumbers[i]);
+    }
 }
+
+//ESERCIZIO 5 ALTRE PROVE
+
+
+for (let even = 0; even < 14; even++) {
+    /* for (inizio; condizione; aggiornamento):
+    inizia dal valore con indice 0, 
+    prosegue fino al valore con indice inferiore a 20 quindi 19, 
+    aggiorna ad ogni ciclo, "++" sarebbe l'abbreviazione di "even + 1",
+    dunque ad ogni giro/ciclo va avanti di 1 ciclo */
+
+    if (even % 2 === 0) {
+        console.log(even);
+
+    }
+}
+
+for (let even = 0; even < 10; even += 2)
+    console.log(even) 
+/* even parte da 0 quindi aggiungendo ogni volta 2 (even += 2) 
+avremo tutti i numeri pari fino a 10 (fino al nono indice) */
+
+
+
+//ESERCIZIO 6 
+
+//numeri dispari fino al 10
+let odd = 1
+
+while(odd <9) {
+    odd += 2;
+    console.log(odd);
+
+}
+
+
+// numeri dispari da 10 a 0
+let odd = 11
+
+while(odd > 0) {
+    odd -= 2;  /* decrementa di 2 ogni giro */    
+    console.log(odd);
+
 }
