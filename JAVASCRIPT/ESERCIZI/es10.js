15a41db5bd6635175da6f182b5bb161b314aa126
Claudiasals <salsiniclaudia@gmail.com>
2025-07-11 14:19:58 +0200

    es.12 switch - verifica il punteggio

--- JAVASCRIPT/ESERCIZI/es10.js
+++ JAVASCRIPT/ESERCIZI/es10.js
@@ -30,27 +30,3 @@ if (score < 59) {
 - Variabili booleane: (isLoggedIn && isAdmin)
 */
 
-//ESERCIZIO 12
-//Switch - Verifica il punteggio
-
-let number = parseInt(prompt("scrivi un numero:"));
-switch (true) {
-    case number <= 100 && number >= 90:
-        console.log("voto ottimo");
-        break; //per farlo fermare quando non trova una corrispondenza
-
-    case number <= 89 && number >= 70:
-        console.log("voto buono");
-        break;
-
-    case number <= 69 && number >= 60:
-        console.log("voto sufficiente");
-        break;
-
-    case
-        number <= 59:
-        console.log("voto insufficiente");
-        break;
-    default:
-        console.log("Numero non valido")
-}
