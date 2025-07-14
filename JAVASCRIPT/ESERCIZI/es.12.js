2bc28920b67e4f29296e8b96a0f8231b03dba753
Claudiasals <salsiniclaudia@gmail.com>
2025-07-14 21:06:43 +0200

    somma array

--- JAVASCRIPT/ESERCIZI/es.12.js
+++ JAVASCRIPT/ESERCIZI/es.12.js
@@ -23,6 +23,20 @@ switch (true) {
         console.log("Numero non valido")
 }
 */
-//ES array di numeri
+//ES array somma di numeri
 let numberArr = [3, 4, 56, 5, 78, 3];
-console.log(numberArr);
+let sum = 0; //var con valore iniziale 0, inizia a 0 perché non abbiamo ancora sommato nientwe. Che terrà conto, sevre a salvare la somma totale degli array
+for (let i = 0; i < numberArr.length; i++) {
+    /* inizializziamo la var i alla pozizione 0; 
+    poi diciamo a i di continuare finché è più piccolo 
+    della lumghezza totale dell'array, ovvero 6;
+    i++ indica come aunentano ad ogni ciclo, ovvero ad ogni 
+    ciclo aumentano di 1, cioè passimo al numero successivo; 
+    Dopo il 6° giro, i diventa 6, ma numberArr.length è 6,quindi 
+    la condizione i < 6 non è più vera, e il ciclo si ferma.*/
+    sum = sum + numberArr[i]; /* aggiungi ogni valore a sum; significa:
+    ➕ prendi il numero che c’è in numberArr[i],
+    ➕ aggiungilo a sum,
+    ➕ e salva il risultato dentro sum di nuovo. */
+}
+console.log(sum);
