58f9f389bbd02dce0df6dabe9907eed4290c3b86
Claudiasals <salsiniclaudia@gmail.com>
2025-07-28 12:29:29 +0200

    es. persistenza delle variabili locali

--- JAVASCRIPT/ESERCIZI/funzioni-annidate.js
+++ JAVASCRIPT/ESERCIZI/funzioni-annidate.js
@@ -1,10 +1,13 @@
 
-function outerFunction(x) {
+function outerFunction(x, initialValue) {
     // outerFunction accetta un parametro x
+    // aggiungo paramero initialValue
+    let result = initialValue;  
+    // memorizza il valore iniziale
     function innerFunction(y) {
         // innerFunction accetta un parametro y
-        return x + y;  
-        // somma x e y e restituisce il risultato
+        return result + y;  
+        // somma result e y e restituisce il risultato
     }
     return innerFunction;  
     // outerFunction restituisce innerFunction
