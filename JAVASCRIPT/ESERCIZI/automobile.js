8eb42d19d8cf1c6ee2100c7a8156a1bd32e022d8
Claudiasals <salsiniclaudia@gmail.com>
2025-07-29 12:55:03 +0200

    automobile

---
+++ JAVASCRIPT/ESERCIZI/automobile.js
@@ -0,0 +1,10 @@
+class automobile {
+    costructor (marca, modello, anno){
+        this.marca=marca;
+        this.modello=modello;
+        this.anno=anno;
+    }
+    }
+descrizione () {
+    return marca: ${this:marca}, modello:${this.modello}, anno:${this.anno}
+}
