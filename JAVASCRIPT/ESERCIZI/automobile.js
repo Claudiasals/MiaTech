8197e9d5975d01cad83d8926953a9f422aeedf13
Claudiasals <salsiniclaudia@gmail.com>
2025-07-29 17:50:10 +0200

    es. classe elettrica automobile

--- JAVASCRIPT/ESERCIZI/automobile.js
+++ JAVASCRIPT/ESERCIZI/automobile.js
@@ -1,18 +1,34 @@
 class Automobile {
-    constructor (marca, modello, anno){
-        this.marca=marca;
-        this.modello=modello;
-        this.anno=anno;
-        this.chilometraggio = 0;     
+    constructor(marca, modello, anno) {
+        this.marca = marca;
+        this.modello = modello;
+        this.anno = anno;
+        this.chilometraggio = 0;
     }
-    
-descrizione () {
-    return ` Marca: ${this.marca}, Modello:${this.modello}, Anno:${this.anno}`;
-}
 
-aggiungiChilometri (km) {
-    this.chilometraggio += km;  // aumento i chilometri
+    descrizione() {
+        return ` Marca: ${this.marca}, Modello:${this.modello}, Anno:${this.anno}`;
+    }
+
+    aggiungiChilometri(km) {
+        this.chilometraggio += km;  // aumento i chilometri
+    }
+    mostraChilometraggio() {
+        return `Chilometraggio: ${this.chilometraggio}`;
+    }       
 }
-mostraChilometraggio() {
-    return `Chilometraggio: ${this.chilometraggio}`;
-}       
+class Elettrica extends Automobile {
+    constructor(marca, modello, anno, km, autonomia) {
+        super(marca, modello, anno, km);
+        this.autonomia = 0;
+    }
+
+    descrizione() {
+        return `Marca: ${this.marca}, Modello:${this.modello}, Anno:${this.anno}, Autonomia:${this.autonomia}`;
+    }
+
+ ricarica(km) {
+        this.autonomia += km;  
+    }
+ 
+}
