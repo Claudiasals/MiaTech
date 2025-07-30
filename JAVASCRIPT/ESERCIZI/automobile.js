bd7f49b817bbb2fcfc5582337015a7a7cb754f51
Claudiasals <salsiniclaudia@gmail.com>
2025-07-30 10:03:16 +0200

    CORRZIONE DI CHAT ES AUTOMABILE

--- JAVASCRIPT/ESERCIZI/automobile.js
+++ JAVASCRIPT/ESERCIZI/automobile.js
@@ -32,3 +32,45 @@ class Elettrica extends Automobile {
     }
  
 }
+
+
+
+//CORREZIONE DI CHATGPT DA VEDERE :
+// Prima parte: Classe Automobile con proprietà e metodi
+class Automobile {
+    constructor(marca, modello, anno) {
+        this.marca = marca;
+        this.modello = modello;
+        this.anno = anno;
+        this.chilometraggio = 0; // proprietà chilometraggio
+    }
+
+    descrizione() {
+        return `Marca: ${this.marca}, Modello: ${this.modello}, Anno: ${this.anno}`;
+    }
+
+    aggiungiChilometri(km) {
+        this.chilometraggio += km;
+    }
+
+    mostraChilometraggio() {
+        return `Chilometraggio: ${this.chilometraggio}`;
+    }
+}
+
+// Seconda parte: Sottoclasse Elettrica estende Automobile
+class Elettrica extends Automobile {
+    constructor(marca, modello, anno, autonomia) {
+        super(marca, modello, anno);
+        this.autonomia = autonomia; // proprietà autonomia
+    }
+
+    descrizione() {
+        // override del metodo per includere anche l'autonomia
+        return `Marca: ${this.marca}, Modello: ${this.modello}, Anno: ${this.anno}, Autonomia: ${this.autonomia} km`;
+    }
+
+    ricarica(km) {
+        this.autonomia += km; // aumenta l'autonomia della batteria
+    }
+}
