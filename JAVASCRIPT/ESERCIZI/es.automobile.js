c9c2ac44d449378f985c6122209d6d497486248a
Claudiasals <salsiniclaudia@gmail.com>
2025-08-01 01:41:10 +0200

    es. metodo protetto

--- JAVASCRIPT/ESERCIZI/es.automobile.js
+++ JAVASCRIPT/ESERCIZI/es.automobile.js
@@ -6,6 +6,18 @@ class Automobile {
         this.chilometraggio = chilometraggio;
     }
 
+
+    /* Metodo protetto
+Aggiungi un metodo protetto _controllaChilometri() alla classe Automobile che verifica 
+se il chilometraggio supera un certo valore (ad esempio 100,000 km) e restituisce 
+un avviso. Utilizza questo metodo nella sottoclasse Elettrica 
+per mostrare un avviso se il chilometraggio supera il limite. */
+
+_controllaChilometri() {
+    return this.chilometraggio > 100000;
+}
+
+
     #calcolaEtà() {
         //anno corrente - this.anno 
       return  new Date().getFullYear() - this.anno
@@ -18,6 +30,9 @@ class Automobile {
         return this.#calcolaEtà();
     }
 
+
+
+   
     descrizione() {
         return `Marca: ${this.marca}, Modello:${this.modello}, Anno:${this.anno}`;
     }
@@ -45,6 +60,17 @@ class Elettrica extends Automobile {
         this.autonomia = autonomia;
     }
 
+
+//perché lo metto qui sotto il pubblico e il protetto nell automobile? 
+overChilometraggio () {
+    if (this._controllaChilometri()) { // if al posto di return se c'è una condizione? 
+        console.warn("Il chilometraggio supera i 100.000 km!"); /* console.warm è una funzione di JS
+         che serve a mostrare un avviso (warning) nella console del browser. */
+         return true;
+        }
+        return false;
+
+    } 
     descrizione() {
         return super.descrizione() + `Autonomia:${this.autonomia}`;
     }
@@ -55,10 +81,10 @@ class Elettrica extends Automobile {
 
 }
 
-/* creo un istanza dalla classe e una dalla sorttoclasse e 
+/* creo un istanza dalla classe e una dalla sottoclasse e 
 verifico con console.log che il metodo saluta funzioni */
 let auto = new Automobile("Fiat", "Panda", 2020, 30000);
-let autoElet = new Elettrica("Tesla", "Model", 2022, 15000, 400);
+let autoElet = new Elettrica("Tesla", "Model", 2022, 150000, 400000);
 
 console.log(auto.saluta()); // Ciao! Sono una Fiat Panda.
 console.log(autoElet.saluta()); // Ciao! Sono una Tesla Model 3.    
@@ -66,3 +92,5 @@ console.log(autoElet.saluta()); // Ciao! Sono una Tesla Model 3.
 
 const miaAuto = new Automobile("Fiat", "Punto", 2018, 25000);
 console.log("Età dell'auto:", miaAuto.mostraEtà());
+
+console.log(autoElet.overChilometraggio());
