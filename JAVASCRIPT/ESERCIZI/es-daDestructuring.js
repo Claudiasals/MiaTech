e4564ed780585857fbbde25ed30c7ec81923b6dd
Claudiasals <salsiniclaudia@gmail.com>
2025-08-13 01:17:13 +0200

    destructuring: stampo le due variabili

--- JAVASCRIPT/ESERCIZI/es-daDestructuring.js
+++ JAVASCRIPT/ESERCIZI/es-daDestructuring.js
@@ -3,12 +3,17 @@ const arrayDiNumeri = [1, 3, 45, 6, 54, 87, 12];
 
 /*Uso il destructuring ([]=nome array o {}= nome obj) 
 per prendere i primi due elementi */
-const [primo, secondo, cacio] = arrayDiNumeri;
+const [primo, , cacio] = arrayDiNumeri;
+/* per saltare un elemento dell'array con il destructuring basta
+lasciare lo spazio vuoto e separarlo con una virgola dagli altri elementi. 
+Questo non serve quando si tratta di oggetti perché le proprietà vengono 
+estratte semplicemente indicandole con il loro nome */
 
 /* con il destructuring js crea già le variabili che inserisci 
 nella parentesi quadra, in questo caso la variabile primo e la variabile secondo, e la variabile cacio
 che prenderanno rispettivamente il primo, il secondo e il terzo numero dell' array
 */
+
 // 3. Stampo le variabili nella console
 console.log(primo);   // 1
 console.log(secondo); // 3
@@ -21,3 +26,6 @@ direttamente in variabili.
 Nel caso degli oggetti non conta l'ordine elle prprietà, 
 quindi il nome della variabile deve corrispondere 
 al nome delle proprietà */
+
+console.log(primo);
+console.log(cacio);
