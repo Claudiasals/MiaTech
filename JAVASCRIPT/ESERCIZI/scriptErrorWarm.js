a891e4c865c26be150f029391eddfe57ef900313
Claudiasals <salsiniclaudia@gmail.com>
2025-08-17 18:24:53 +0200

    es. console.table e .group

--- JAVASCRIPT/ESERCIZI/scriptErrorWarm.js
+++ JAVASCRIPT/ESERCIZI/scriptErrorWarm.js
@@ -12,3 +12,19 @@ console.error ("Errore") //errore xke il risultato non è 14
 } else if (isNaN(numbers)) { // senon è un numero
     console.warn("Attenzione: puoi inserire solo numeri."); 
   } 
+
+  //es. console.table
+
+  let persone = [
+    { nome: "Anna", eta: 25, città: "Milano" },
+    { nome: "Luca", eta: 30, città: "Roma" },
+    { nome: "Piero", eta: 68, città: "Torino" }
+];
+
+console.table(persone); 
+
+//console.group
+console.group("Messaggi di debug");
+console.log("Primo messaggio dentro al gruppo");
+console.log("Secondo messaggio dentro al gruppo");
+console.groupEnd();
