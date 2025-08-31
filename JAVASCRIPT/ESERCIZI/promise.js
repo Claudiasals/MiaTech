65a0189233bb4c30520e53131e839b06ca2ec5dd
Claudiasals <salsiniclaudia@gmail.com>
2025-09-01 01:07:10 +0200

    Es. finally

--- JAVASCRIPT/ESERCIZI/promise.js
+++ JAVASCRIPT/ESERCIZI/promise.js
@@ -35,3 +35,31 @@ attendiCaricamento
     .catch(errore => {
         console.error("Errore:", errore);
     });
+
+
+
+
+//ES.FINALLY
+
+function finallyPromise() {
+    return new Promise((resolve, reject) => {
+        let successo = true; 
+        if (successo) {
+            resolve("Resolve");
+        } else {
+            reject("Reject");
+        }
+    });
+}
+
+// Utilizzo della promessa con finally
+finallyPromise()
+    .then(risultato => {
+        console.log(risultato);
+    })
+    .catch(errore => {
+        console.error(errore);
+    })
+    .finally(() => {
+        console.log("Finally: questa azione viene eseguita sempre, indipendentemente dal risultato");
+    });
