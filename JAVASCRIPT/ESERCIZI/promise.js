8dd32d21e804a147149831ec18df7c56a71528b5
Claudiasals <salsiniclaudia@gmail.com>
2025-09-01 01:21:29 +0200

    catena di promesse (.then)

--- JAVASCRIPT/ESERCIZI/promise.js
+++ JAVASCRIPT/ESERCIZI/promise.js
@@ -43,7 +43,7 @@ attendiCaricamento
 
 function finallyPromise() {
     return new Promise((resolve, reject) => {
-        let successo = true; 
+        let successo = true;
         if (successo) {
             resolve("Resolve");
         } else {
@@ -62,4 +62,29 @@ finallyPromise()
     })
     .finally(() => {
         console.log("Finally: questa azione viene eseguita sempre, indipendentemente dal risultato");
+    });
+
+
+
+//ES. CATENA DI PROMESSE
+
+// Funzione che restituisce una promessa che si risolve dopo 1 secondo con un numero
+function getNumber() {
+    return new Promise(function(resolve) {
+      setTimeout(function() {
+        resolve(5); // restituisce 5 dopo 1 secondo
+      }, 1000);
+    });
+  }
+  
+  // Catena di promesse
+  getNumber()
+    .then(function(num) {
+      return num * 2; // prendi il valore e moltiplica per 2
+    })
+    .then(function(num) {
+      return num + 3; // prendi il valore e aggiungi 3
+    })
+    .then(function(result) {
+      console.log("Risultato finale:", result); 
     });
