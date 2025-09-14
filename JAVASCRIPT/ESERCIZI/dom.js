493b6a1e2d55e20c9ace5cfc27f63aa06a3aa63a
Claudiasals <salsiniclaudia@gmail.com>
2025-09-15 01:12:17 +0200

    ESERCIZIO NUMERO 3: utilizza querySelector

--- JAVASCRIPT/ESERCIZI/dom.js
+++ JAVASCRIPT/ESERCIZI/dom.js
@@ -1,2 +1,9 @@
 // Seleziona l'elemento con id "paragraph"
 const paragrafo = document.getElementById("paragraph");
+
+ // 🔹 Cos'è querySelector?
+        // querySelector è un metodo del DOM che permette di selezionare
+        // un elemento che corrisponde a un selettore CSS.
+        // Esempio: "#id" per id, ".classe" per classi, "tag" per tag HTML.
+
+        const lista = document.querySelector("#list");
